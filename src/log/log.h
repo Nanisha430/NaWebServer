f3e03b98536606e3bbc8a5909679d465af6d92cb
20424989 <542297096@qq.com>
2023-08-18 10:55:36 +0800

    fix: log bug

--- src/log/log.h
+++ src/log/log.h
@@ -1,3 +1,8 @@
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-16
+ * @copyleft GPL 2.0
+ */ 
 #ifndef LOG_H
 #define LOG_H
 
@@ -6,77 +11,65 @@
 #include <thread>
 #include <sys/time.h>
 #include <string.h>
-#include <vector>
 #include <stdarg.h> // vastart va_end
 #include <assert.h>
 #include "blockqueue.h"
 
-class Log {
+class Log 
+{
 public:
-  void init(const char *path = "./log", const char *suffix = ".log",
-            int buffSize = 8192, int maxLines = 5000000,
-            int maxQueueCapacity = 800);
-  static Log *GetInstance() {
-    static Log inst;
-    return &inst;
-  }
+    void init(const char* path = "./log", const char* suffix =".log",
+        int buffSize = 8192, int maxLines = 5000000,
+        int maxQueueCapacity = 800);
 
-  static void FlushLogThread() { Log::GetInstance()->AsyncWrite_(); }
+    static Log* GetInstance();
 
-  void write(int level, const char *format, ...);
-  void flush();
+    static void FlushLogThread();
 
-private:
-  Log();
-  virtual ~Log();
-  void AsyncWrite_();
+    void write(int level, const char *format,...);
+
+    void flush();
 
 private:
-  const static int LOG_PATH_LEN = 128;
-  const static int LOG_NAME_LEN = 256;
+    Log();
+    virtual ~Log();
+    void AsyncWrite_();
 
-  char path_[LOG_PATH_LEN];
-  char suffix_[LOG_NAME_LEN];
+private:
+    static const int LOG_PATH_LEN = 128;
+    static const int LOG_NAME_LEN = 256;
+    
+    char path_[LOG_PATH_LEN];
+    char suffix_[LOG_NAME_LEN];
 
-  int MAX_LINE;
-  int BUFF_SIZE;
+    int MAX_LINE;
+    int BUFF_SIZE;
 
-  int lineCount_;
-  int toDay_;
+    int lineCount_;
+    int toDay_;
 
-  FILE *fp_;
-  char *buffer_;
+    FILE* fp_;
+    char* buffer_;
+    
+    BlockDeque<std::string> *deque_; 
 
-  BlockDeque<std::string> *deque_;
-  std::mutex mtx_;
-  bool isAsync_;
-  std::thread *writePID_;
+    std::mutex mtx_;
+    bool isAsync_;
+    std::thread *writePID_;
 };
 
-#define LOG_BASE(level, format, ...)                                           \
-  do {                                                                         \
-    if (0 == this->IsCloseLog()) {                                             \
-      Log::GetInstance()->write(level, format, ##__VA_ARGS__);                 \
-      Log::GetInstance()->flush();                                             \
-    }                                                                          \
-  } while (0);
 
-#define LOG_DEBUG(format, ...)                                                 \
-  do {                                                                         \
-    LOG_BASE(0, format, ..., ##__VA_ARGS__)                                    \
-  } while (0);
+#define LOG_BASE(level, format, ...) \
+    do {\
+        if (0 == this->IsCloseLog()) {\
+            Log::GetInstance()->write(level, format, ##__VA_ARGS__); \
+            Log::GetInstance()->flush();\
+        }\
+    } while(0);
 
-#define LOG_INFO(format, ...)                                                  \
-  do {                                                                         \
-    LOG_BASE(0, format, ..., ##__VA_ARGS__)                                    \
-  } while (0);
-#define LOG_WARN(format, ...)                                                  \
-  do {                                                                         \
-    LOG_BASE(0, format, ..., ##__VA_ARGS__)                                    \
-  } while (0);
-#define LOG_ERROR(format, ...)                                                 \
-  do {                                                                         \
-    LOG_BASE(0, format, ..., ##__VA_ARGS__)                                    \
-  } while (0);
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while(0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while(0);
 
-#endif
+#endif //LOG_H
--- src/pool/sqlconnpool.cpp
+++ src/pool/sqlconnpool.cpp
@@ -1,4 +1,14 @@
+/*
+ * @Author       : mark
+ * @Date         : 2020-06-17
+ * @copyleft GPL 2.0
+ */
+
 #include "sqlconnpool.h"
+#include <cstddef>
+#include <mutex>
+#include <mysql/mysql.h>
+#include <semaphore.h>
 
 SqlConnPool::SqlConnPool() {
   useCount_ = 0;
@@ -9,9 +19,74 @@ SqlConnPool *SqlConnPool::GetInstance() {
   static SqlConnPool connPool;
   return &connPool;
 }
+
 void SqlConnPool::Init(std::string host, std::string user, std::string pwd,
                        std::string dbName, int port, int connSize,
                        bool isCloseLog) {
   sqlConfig_ = {host, user, pwd, dbName, port};
   isCloseLog_ = isCloseLog;
+  for (int i = 0; i < connSize; i++) {
+    MYSQL *sql = nullptr;
+    mysql_init(sql);
+    if (!sql) {
+      LOG_ERROR("mysql init error");
+      exit(1);
+    }
+    sql = mysql_real_connect(sql, sqlConfig_.host.c_str(),
+                             sqlConfig_.user.c_str(), sqlConfig_.pwd.c_str(),
+                             sqlConfig_.dbName.c_str(), sqlConfig_.port,
+                             nullptr, 0);
+
+    if (!sql) {
+      LOG_ERROR("mysql connect error");
+      exit(1);
+    }
+    connQue_.push(sql);
+  }
+  MAX_CONN_ = connSize;
+  sem_init(&semId_, 0, MAX_CONN_);
 }
+
+MYSQL *SqlConnPool::GetConn() {
+  MYSQL *sql = nullptr;
+  if (connQue_.empty()) {
+    LOG_WARN("sqlConnpool busy");
+    return nullptr;
+  }
+  {
+    std::lock_guard<std::mutex> locker(mtx_);
+    sql = connQue_.front();
+    connQue_.pop();
+  }
+  return sql;
+}
+
+void SqlConnPool::FreeConn(MYSQL *conn) {
+  {
+    if (conn) {
+      std::lock_guard<std::mutex> locker(mtx_);
+      connQue_.push(conn);
+      sem_post(&semId_);
+    }
+  }
+}
+
+void SqlConnPool::ClosePool() {
+  {
+    std::lock_guard<std::mutex> locker(mtx_);
+    while (!connQue_.empty()) {
+      auto item = connQue_.front();
+      connQue_.pop();
+      mysql_close(item);
+    }
+  }
+}
+
+int SqlConnPool::GetFreeConnCount() {
+  {
+    std::lock_guard<std::mutex> locker(mtx_);
+    return connQue_.size();
+  }
+}
+
+SqlConnPool::~SqlConnPool() { ClosePool(); }
