a0d2c8b924cbd67e83f34c24644812e342c280c8
20424989 <542297096@qq.com>
2023-08-29 17:46:22 +0800

    httpconn

--- code/http/httpconn.cpp
+++ code/http/httpconn.cpp
@@ -1,5 +1,52 @@
 #include "httpconn.h"
+#include <cassert>
 
 const char *HttpConn::srcDir;
 std::atomic<int> HttpConn::userCount;
 bool HttpConn::isET;
+
+HttpConn::HttpConn() {
+    fd_ = -1;
+    addr_ = {0};
+    isClose_ = true;
+}
+
+HttpConn::~HttpConn() {
+    Close();
+}
+
+void HttpConn::init(int sockFd, const sockaddr_in &addr) {
+    assert(sockFd > 0);
+    userCount++;
+    fd_ = sockFd;
+    addr_ = addr;
+    isClose_ = false;
+    readBuff_.RetrieveAll();
+    writeBuff_.RetrieveAll();
+    LOG_INFO("Client[%d](%s:%d) in, userCount:%d", fd_, GetIP(), GetPort(), (int)userCount);
+}
+
+void HttpConn::Close() {
+    response_.UnmapFile();
+    if (isClose_ == false) {
+        isClose_ = true;
+        close(fd_);
+        userCount--;
+        LOG_INFO("Client[%d](%s:%d) quit, UserCount:%d", fd_, GetIP(), GetPort(), (int)userCount);
+    }
+}
+int HttpConn::GetFd() const {
+    return fd_;
+};
+
+struct sockaddr_in HttpConn::GetAddr() const {
+    return addr_;
+}
+
+const char *HttpConn::GetIP() const {
+    return inet_ntoa(addr_.sin_addr);
+}
+
+int HttpConn::GetPort() const {
+    return addr_.sin_port;
+}
